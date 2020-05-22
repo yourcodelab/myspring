2faa35002bc485f5d663d3951f014c89f3d1515d
yourcodelab <alexssander_as@hotmail.com>
2020-05-22 14:20:11 -0300

    Adding RDS integration.

--- src/main/java/com/example/myspring/Customer.java
+++ src/main/java/com/example/myspring/Customer.java
@@ -1,17 +1,21 @@
 package com.example.myspring;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "TB_CUSTOMERS", schema = "cloud")
 public class Customer{
-    @Id
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-
+	@Column(name = "first_name")
 	private String firstName;
+	@Column(name = "last_name")
 	private String lastName;
 
     public Customer(String firstName, String lastName){
--- src/main/java/com/example/myspring/DemoApplication.java
+++ src/main/java/com/example/myspring/DemoApplication.java
@@ -1,26 +1,11 @@
 package com.example.myspring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-	@Autowired
-	private CustomerRepository repository;
-
-	@Bean
-	public void run(){
-		repository.save(new Customer("Jack", "Bauer"));
-		repository.save(new Customer("Chloe", "O'Brian"));
-		repository.save(new Customer("Kim", "Bauer"));
-		repository.save(new Customer("David", "Palmer"));
-		repository.save(new Customer("Michelle", "Dessler"));
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
